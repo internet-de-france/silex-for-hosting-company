0268cd87d78b36cfe9d8df0f77f21959c10ef163
Alex Hoyau <a.hoyau@silexlabs.org>
2018-09-13 17:54:15 +0200

    files for heroku

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ config.publisherOptions.skipProviderSelection = true;
 config.publisherOptions.enableGithubPages = false;
 console.log('publisher config:', config.publisherOptions);
 
-config.ceOptions.githubClientId = "f124e4148bf9d633d58b";
-config.ceOptions.githubClientSecret = "1a8fcb93d5d0786eb0a16d81e8c118ce03eefece";
+// hardcode for test, should be in env vars
+config.ceOptions.githubClientId = 'be5c0bfaa1c0438074c7';
+config.ceOptions.githubClientSecret = '7b456fec8e1dd9874b8573b703cc44b693a27d89';
 console.log('cloud explorer config:', config.ceOptions);
 
 const silex = new SilexServer(config);
