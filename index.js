146352c7d245d22df74fbed0ab392caa54e93444
Alex Hoyau <a.hoyau@silexlabs.org>
2018-09-18 12:48:23 +0200

    github app creds in env vars

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const CustomProvider = require('./custom-provider');
 
 config.publisherOptions.skipProviderSelection = true;
 config.publisherOptions.enableGithubPages = false;
-console.log('publisher config:', config.publisherOptions);
-
-// hardcode for test, should be in env vars
-config.ceOptions.githubClientId = 'be5c0bfaa1c0438074c7';
-config.ceOptions.githubClientSecret = '7b456fec8e1dd9874b8573b703cc44b693a27d89';
-console.log('cloud explorer config:', config.ceOptions);
 
 const silex = new SilexServer(config);
 //silex.unifile.use(new CustomService());
